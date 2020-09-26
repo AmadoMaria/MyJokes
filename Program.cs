ea63689112ad1acdbac06de6a06ee0d909606cbd
AmadoMaria <mariantoniamado@hotmail.com>
2020-09-25 21:18:17 -0300

    Iniciando projeto

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MyJokes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetData();
+            Console.ReadKey();
+        }
+
+        public static async void GetData()
+        {
+            HttpClient httpClient = new HttpClient();
+            HttpRequestMessage request = new
+            HttpRequestMessage(HttpMethod.Get,
+           "http://jsonplaceholder.typicode.com/posts/1");
+            HttpResponseMessage response = await
+            httpClient.SendAsync(request);
+            string dados = await
+            response.Content.ReadAsStringAsync();
+
+            //Post p = JsonConvert.DeserializeObject<Post>(dados);
+            //Console.WriteLine($"Titulo: {p.Title}");
+           // Console.WriteLine($"Body: {p.Body}");
+
+        }
+    }
+}
