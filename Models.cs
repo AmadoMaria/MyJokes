4f415761eb0207048105607d8ae4392710393c3c
AmadoMaria <mariantoniamado@hotmail.com>
2020-09-25 21:56:30 -0300

    consuming JokeAPI

--- Models.cs
+++ Models.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace MyJokes
 {
-    class Models
+    public class Flags
     {
+        public bool nsfw { get; set; }
+        public bool religious { get; set; }
+        public bool political { get; set; }
+        public bool racist { get; set; }
+        public bool sexist { get; set; }
+    }
+
+    public class JokeObj
+    {
+        public bool error { get; set; }
+        public string category { get; set; }
+        public string type { get; set; }
+        public string joke{ get; set; }
+        public string setup { get; set; }
+        public string delivery { get; set; }
+        public Flags flags { get; set; }
+        public int id { get; set; }
+        public string lang { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,25 +9,72 @@ namespace MyJokes
     {
         static void Main(string[] args)
         {
-            GetData();
+            JokeObj joke = new JokeObj();
+            Console.WriteLine("Please, select a category:");
+            Console.WriteLine("[0] Any category");
+            Console.WriteLine("[1] Programming");
+            Console.WriteLine("[2] Miscellaneous");
+            Console.WriteLine("[3] Dark");
+            Console.WriteLine("[4] Pun");
+
+            string category = Console.ReadLine();
+
+            switch (category)
+            {
+                case "0":
+                    joke.category = "Any";
+                    break;
+                case "1":
+                    joke.category = "Programming";
+                    break;
+                case "2":
+                    joke.category = "Miscellaneous";
+                    break;
+                case "3":
+                    joke.category = "Dark";
+                    break;
+                case "4":
+                    joke.category = "Pun";
+                    break;
+                default:
+                    Console.WriteLine("Código inválido");
+                    break;
+            }
+
+            GetData(joke);
             Console.ReadKey();
         }
 
-        public static async void GetData()
+        public static void PrintJoke(JokeObj joke)
+        {
+            if (joke.type == "single")
+            {
+                Console.WriteLine($"Category: {joke.category}");
+                Console.WriteLine($"joke: {joke.joke}");
+            }
+            else
+            {
+                Console.WriteLine($"Category: {joke.category}");
+                Console.WriteLine($"Setup: {joke.category}");
+                Console.WriteLine($"Delivery: {joke.category}");
+            }
+      
+        }
+
+        public static async void GetData(JokeObj joke)
         {
             HttpClient httpClient = new HttpClient();
+            string url = 
+               $"https://sv443.net/jokeapi/v2/joke/{joke.category}?blacklistFlags=nsfw,religious,political,racist,sexist";
             HttpRequestMessage request = new
-            HttpRequestMessage(HttpMethod.Get,
-           "http://jsonplaceholder.typicode.com/posts/1");
+            HttpRequestMessage(HttpMethod.Get, url);
             HttpResponseMessage response = await
             httpClient.SendAsync(request);
-            string dados = await
+            string data = await
             response.Content.ReadAsStringAsync();
 
-            //Post p = JsonConvert.DeserializeObject<Post>(dados);
-            //Console.WriteLine($"Titulo: {p.Title}");
-           // Console.WriteLine($"Body: {p.Body}");
-
+            joke = JsonConvert.DeserializeObject<JokeObj>(data);
+            PrintJoke(joke);
         }
     }
 }
